0d196054fea3e155702ef5fa0ac8fde6a04c1e8b
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2025-04-29 08:50:00 +0900

    Update load_lambada.py

--- load_lambada.py
+++ load_lambada.py
@@ -1,4 +1,17 @@
 from datasets import load_dataset
-
 lambada = load_dataset("lambada")
 print(lambada)
+# => DatasetDict({
+#     train: Dataset({
+#         features: ['text', 'domain'],
+#         num_rows: 2662
+#     })
+#     test: Dataset({
+#         features: ['text', 'domain'],
+#         num_rows: 5153
+#     })
+#     validation: Dataset({
+#         features: ['text', 'domain'],
+#         num_rows: 4869
+#     })
+# })
