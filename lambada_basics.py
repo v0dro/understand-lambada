f2574feacdaf052fa86c49d8422f43f935129ad4
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2025-04-29 10:20:40 +0900

    Stats for test and validation datasets

--- lambada_basics.py
+++ lambada_basics.py
@@ -23,3 +23,33 @@ print(f"""LAMBADA training dataset stats:
 #   Std. dev. of words per novel: 44594.20095395589
 #   Minimum words per novel:      32
 #   Maximum words per novel:      429820
+
+test_dataset = lambada["test"]
+# Display some stats about the training dataset
+print(f"Fields in each entry of the training dataset: {test_dataset[0].keys()}")
+word_counts = list()
+for example in test_dataset:
+    text_len = len(example["text"].split())
+    if text_len > 0: word_counts.append(text_len) 
+
+print(f"""LAMBADA test dataset stats:
+  Average words per entry:      {statistics.mean(word_counts)}
+  Std. dev. of words per entry: {statistics.stdev(word_counts)}
+  Minimum words per entry:      {min(word_counts)}
+  Maximum words per entry:      {max(word_counts)}
+""")
+
+validation_dataset = lambada["test"]
+# Display some stats about the training dataset
+print(f"Fields in each entry of the training dataset: {validation_dataset[0].keys()}")
+word_counts = list()
+for example in validation_dataset:
+    text_len = len(example["text"].split())
+    if text_len > 0: word_counts.append(text_len) 
+
+print(f"""LAMBADA validation dataset stats:
+  Average words per entry:      {statistics.mean(word_counts)}
+  Std. dev. of words per entry: {statistics.stdev(word_counts)}
+  Minimum words per entry:      {min(word_counts)}
+  Maximum words per entry:      {max(word_counts)}
+""")
